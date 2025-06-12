a47af974e943799c19d6a2209406cccf90f4b37c
JoseluisDev24 <161613858+JoseluisDev24@users.noreply.github.com>
2025-06-12 13:37:49 -0300

    rename mew img in filterPokemon file

--- src/data/filterPokemon.ts
+++ src/data/filterPokemon.ts
@@ -3,7 +3,7 @@ export const filterPokemon = [
     { name: "Blastoise", image: "/pokemon/blastoise.avif" },
     { name: "Gengar", image: "/pokemon/Gengar.avif" },
     { name: "Lugia", image: "/pokemon/lugia.avif" },
-    { name: "Mew", image: "/pokemon/Mew.avif" },
+    { name: "Mew", image: "/pokemon/mew.avif" },
     { name: "Mewtwo", image: "/pokemon/mewtwo.avif" },
     { name: "Pikachu", image: "/pokemon/pikachu.avif" },
     { name: "Rayquaza", image: "/pokemon/Rayquaza.avif" },
