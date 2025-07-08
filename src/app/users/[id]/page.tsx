0a917f5e1b3b9ca582115bdddc714464200cc355
JoseluisDev24 <161613858+JoseluisDev24@users.noreply.github.com>
2025-07-07 22:53:47 -0300

    styles and listings for user id page

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -22,20 +22,35 @@ export default async function UserPage({ params }: Props) {
     .filter((card): card is Card => Boolean(card));
 
   return (
-    <div className="p-6">
-      <div className="flex items-center gap-4 mb-4">
-        <img
-          src={user.avatar}
-          alt={user.name}
-          className="w-14 h-14 rounded-full border"
-        />
-        <h1 className="text-2xl font-bold">{user.name}</h1>
+    <div className="lg:p-6">
+      <div className="flex gap-4 mb-4 px-2 lg:px-20 py-4 h-20 lg:h-36 md:rounded-t-4xl bg-blue-200  relative">
+        <div className="flex flex-col justify-center items-center lg:gap-4 absolute bottom-[-150%] lg:bottom-[-100%]">
+          <img
+            src={user.avatar}
+            alt={user.name}
+            className="w-22 h-22 lg:w-44 lg:h-44 rounded-full border-white border-4"
+          />
+          <h1 className="text-xl lg:hidden font-bold">{user.name}</h1>
+          <div className="text-lg md:text-xl font-semibold flex gap-4 lg:gap-8 justify-between">
+            <div className="flex flex-col items-center justify-center text-center">
+              <span>{user.listings}</span>
+              <span className="text-sm text-gray-600"> listings</span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center">
+              <span>{user.trades}</span>
+              <span className="text-sm text-gray-600">Trades</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="lg:flex justify-evenly">
+      <div className="h-36 border-b border-gray-300 relative">
+        <div className="absolute lg:left-1/4">
+          {/* <h1 className="text-2xl lg:text-4xl font-bold">{user.name}</h1> */}
+        </div>
+      </div>
+      <div className="lg:flex justify-evenly p-4">
         <section className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">
-            My Cards
-          </h2>
+          <h2 className="text-xl font-semibold mb-2">My Cards</h2>
           <div className="grid grid-cols-3 md:grid-cols-4 gap-2 lg:gap-y-6 max-w-xl">
             {userFullCards.map((card) => (
               <div
